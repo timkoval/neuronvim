c79fd5f61f2f68c75b29c284aa8ca2d5fc70c514
Tim Koval <timkoval00@gmail.com>
2024-05-29 14:23:19 +0500

    fix(plugins): rearranged some

--- init.lua
+++ init.lua
@@ -130,21 +130,7 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  import = 'custom/plugins',
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
-  -- Use `opts = {}` to force a plugin to be loaded.
-  --
-  --  This is equivalent to:
-  --    require('Comment').setup({})
-
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  import = 'custom/plugins'
 }, {
   -- lazy settings
   change_detection = {
---
+++ lua/custom/plugins/temp.lua
@@ -0,0 +1,5 @@
+return {
+  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+}
