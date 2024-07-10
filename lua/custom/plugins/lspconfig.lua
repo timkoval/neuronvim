8a69fc15fe0ff7a1a4b52420e76c79d82e490874
Tim Koval <timkoval00@gmail.com>
2024-07-10 18:08:59 +0500

    feat(lsp): rust inlay hints

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -159,7 +159,11 @@ return {
         clangd = {},
         -- gopls = {},
         pyright = {},
-        rust_analyzer = {},
+        rust_analyzer = {
+          settings = {
+            inlayHints = { enable = true },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
