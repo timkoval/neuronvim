056368b48ec44943cc9ff23040ea086874dd2ceb
Tim Koval <timkoval00@gmail.com>
2024-10-25 00:12:37 +0400

    add: rustaceanvim config to exclude .direnv and target dirs

--- lua/custom/plugins/rustaceanvim.lua
+++ lua/custom/plugins/rustaceanvim.lua
@@ -5,4 +5,18 @@ return {
   'mrcjkb/rustaceanvim',
   version = '^5', -- Recommended
   lazy = false, -- This plugin is already lazy
+  init = function()
+    -- Configure rustaceanvim here
+    vim.g.rustaceanvim = {
+      server = {
+        default_settings = {
+          ['rust-analyzer'] = {
+            files = {
+              excludeDirs = { './target', './.direnv' },
+            },
+          },
+        },
+      },
+    }
+  end,
 }
