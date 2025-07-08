2c830c0ac845908204a74907ee75b4c8b3320345
Tim <timkoval00@gmail.com>
2025-07-08 15:24:46 +0400

    add: tmux integration

--- lua/custom/plugins/test.lua
+++ lua/custom/plugins/test.lua
@@ -1,6 +1,9 @@
 -- Testing
 return {
   'vim-test/vim-test',
+  dependencies = {
+    'preservim/vimux', -- for running tests in tmux
+  },
   keys = {
     { '<Leader>rs', ':w<CR>:TestSuite<CR>' },
     { '<Leader>rf', ':w<CR>:TestFile<CR>' },
@@ -8,16 +11,13 @@ return {
     { '<Leader>rn', ':w<CR>:TestNearest<CR>' },
     { '<Leader>rv', ':w<CR>:TestVisit<CR>' },
   },
-  init = function()
-    vim.g.tslime = {
-      session = 'runner',
-      window = '1',
-      pane = '1',
-    }
-
-    vim.g['test#strategy'] = 'floaterm'
-
-    vim.g['test#php#phpunit#executable'] = 'vendor/bin/phpunit'
+  config = function()
+    vim.keymap.set('n', '<Leader>rs', ':w<CR>:TestSuite<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<Leader>rf', ':w<CR>:TestFile<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<Leader>rl', ':w<CR>:TestLast<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<Leader>rn', ':w<CR>:TestNearest<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<Leader>rv', ':w<CR>:TestVisit<CR>', { noremap = true, silent = true })
+    vim.cmd "let test#strategy = 'vimux'" -- Use vimux for running tests in tmux
   end,
 }
 -- vim: ts=2 sts=2 sw=2 et
