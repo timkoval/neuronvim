999dcfe1914055c0411cc4ed2be0cb038d112e36
Tim <timkoval00@gmail.com>
2025-08-18 14:09:58 +0400

    fix: fff

--- lua/custom/plugins/fff.lua
+++ lua/custom/plugins/fff.lua
@@ -2,19 +2,19 @@
 -- Description: A fast file finder for Neovim, written in Rust.
 return {
   'dmtrKovalenko/fff.nvim',
-  build = 'cargo build --release',
+  -- build = 'cargo build --release',
   -- or if you are using nixos
-  -- build = "nix run .#release",
+  build = 'nix run .#release',
   opts = {
     -- pass here all the options
   },
   keys = {
     {
-      'sf', -- try it if you didn't it is a banger keybinding for a picker
+      '<leader>sf', -- try it if you didn't it is a banger keybinding for a picker
       function()
         require('fff').find_files() -- or find_in_git_root() if you only want git files
       end,
-      desc = 'Open file picker',
+      desc = '[S]earch [F]iles',
     },
   },
 }
