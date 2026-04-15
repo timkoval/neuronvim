a31cb3b9022296990c3978b929f56a5a9ba22ed7
Tim Koval <timkoval00@gmail.com>
2026-04-15 15:01:00 +0400

    fix: warnings in keybindings

--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -21,26 +21,17 @@ return {
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
+      require('which-key').add {
         { "<leader>c", group = "[C]ode" },
-        { "<leader>c_", hidden = true },
         { "<leader>d", group = "[D]ocument" },
-        { "<leader>d_", hidden = true },
         { "<leader>h", group = "Git [H]unk" },
-        { "<leader>h_", hidden = true },
         { "<leader>r", group = "[R]ename" },
-        { "<leader>r_", hidden = true },
         { "<leader>s", group = "[S]earch" },
-        { "<leader>s_", hidden = true },
         { "<leader>t", group = "[T]oggle" },
-        { "<leader>t_", hidden = true },
         { "<leader>w", group = "[W]orkspace" },
-        { "<leader>w_", hidden = true },
-      }
-      -- visual mode
-      require('which-key').register({
+        -- visual mode
         { "<leader>h", desc = "Git [H]unk", mode = "v" },
-      })
+      }
     end,
   },
 }
